1c73ad06f1d9889aa251f984f9a017512e68743a
tubakaradeniz <82728049+tubakaradeniz@users.noreply.github.com>
2021-05-16 18:58:51 +0300

    Delete src directory

--- src/abstracts/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package abstracts;
-
-import entities.Customer;
-
-public interface CustomerService {
-
-	public void save(Customer customer) ;
-
-}
--- src/concrete/NeroCustomerManager.java
+++
@@ -1,7 +0,0 @@
-package concrete;
-
-import abstracts.BaseCustomerManager;
-
-public class NeroCustomerManager extends BaseCustomerManager{
-
-}
